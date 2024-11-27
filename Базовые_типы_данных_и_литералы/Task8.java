05d57b16fea5872be3b2a941be7ca5f3d50ced52
Arina <137212465+arinauru@users.noreply.github.com>
2024-11-27 22:54:51 +0300

    Update Task8.java

--- Базовые_типы_данных_и_литералы/Task8.java
+++ Базовые_типы_данных_и_литералы/Task8.java
@@ -3,7 +3,6 @@ public class Task8 {
         var number = 42; // Тип переменной будет int
         System.out.println("Тип переменной number: " + ((Object) number).getClass().getSimpleName());
 
-        // Выведение типа для строки
         var greeting = "Hello, world!"; // Тип переменной будет String
         System.out.println("Тип переменной greeting: " + ((Object) greeting).getClass().getSimpleName());
     }
